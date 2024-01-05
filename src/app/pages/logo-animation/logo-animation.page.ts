a4906f5d04d9b2d30296b98debc36e120c93e6dd
amiranm <amira.nagahmahrous.xentra@gmail.com>
2024-01-06 00:42:42 +0800

    page navigation on logo-animation to splash-screen fixed font size in featured-band removed buttons from ion-footer

--- src/app/pages/logo-animation/logo-animation.page.ts
+++ src/app/pages/logo-animation/logo-animation.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-logo-animation',
   templateUrl: './logo-animation.page.html',
@@ -7,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoAnimationPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private navController: NavController, private router: Router) { }
+  navigateForward() {
+    this.navController.setDirection('forward');
+    this.router.navigate(['/home']);
+}
+ionViewWillEnter(){
+  setTimeout(() => {
+    this.navController.navigateForward(['/splash-screen'])
+  }, 5500);
+}
   ngOnInit() {
   }
 
